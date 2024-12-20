5e18089d1ae5f22df3a65964a2f6e43a95dd59cc
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-12-20 17:07:05 +0100

    get paris data instead of lyon

--- script/get-data.sh
+++ script/get-data.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
 # Download the data
-curl -s -o ./data/listings.csv.gz https://data.insideairbnb.com/france/auvergne-rhone-alpes/lyon/2024-09-13/data/listings.csv.gz 
-curl -s -o ./data/calendar.csv.gz https://data.insideairbnb.com/france/auvergne-rhone-alpes/lyon/2024-09-13/data/calendar.csv.gz 
+curl -s -o ./data/listings.csv.gz https://data.insideairbnb.com/france/ile-de-france/paris/2024-09-06/data/listings.csv.gz 
+curl -s -o ./data/calendar.csv.gz https://data.insideairbnb.com/france/ile-de-france/paris/2024-09-06/data/calendar.csv.gz
 # Unzip the data
 gunzip ./data/listings.csv.gz
 gunzip ./data/calendar.csv.gz
