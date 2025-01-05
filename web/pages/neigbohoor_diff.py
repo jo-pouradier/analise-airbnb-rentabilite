4c6269656957f3c14dfaa992ee8ef97c33cb8749
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2025-01-05 16:03:18 +0100

    refactor: improve data merging and mapping for neighborhood analysis

--- web/pages/neigbohoor_diff.py
+++ web/pages/neigbohoor_diff.py
@@ -15,12 +15,10 @@ dash.register_page(__name__, path="/map_arrondissements")
 data_path = os.path.join(os.path.dirname(__file__), '..','..', 'data', 'pondered_listings_process.csv')
 # data1 = pd.read_csv('./../../data/pondered_listings_process.csv')
 data1 = pd.read_csv(data_path)
-data1 = data1.replace([np.inf, -np.inf], np.nan).dropna(subset=['price_per_m2', 'longitude', 'latitude'])
 
 # Load your second dataset with coordinates and prices
 data_path2 = os.path.join(os.path.dirname(__file__), '..','..', 'data', 'loyer_process.csv')
 data2 = pd.read_csv(data_path2)
-data2 = data2.replace([np.inf, -np.inf], np.nan).dropna(subset=['price_per_m2', 'geo_point_2d'])
 
 # Convert the coordinates to GeoDataFrames
 geometry1 = [Point(xy) for xy in zip(data1.longitude, data1.latitude)]
@@ -34,8 +32,6 @@ geo_data2 = gpd.GeoDataFrame(data2, geometry=geometry2)
 shapefile_path = os.path.join(os.path.dirname(__file__), '..','..', 'data','arrondissements', 'arrondissements.shp')
 gdf = gpd.read_file(shapefile_path)
 
-
-
 # Perform the spatial join to get the arrondissement for each point
 geo_data1 = gpd.sjoin(geo_data1, gdf, how='left')
 geo_data2 = gpd.sjoin(geo_data2, gdf, how='left')
@@ -45,6 +41,9 @@ geo_data2 = gpd.sjoin(geo_data2, gdf, how='left')
 avg_price1 = geo_data1.groupby('index_right')['price_per_m2_pondered'].mean().reset_index()
 avg_price1.columns = ['index_right', 'price_per_m2_pondered']
 
+avg_price1_basics = geo_data1.groupby('index_right')['price_per_m2'].mean().reset_index()
+avg_price1_basics.columns = ['index_right', 'price_per_m2']
+
 # Calculate the average availability per neighborhood for listings
 avg_availability = geo_data1.groupby('index_right')['percentage_unavailable'].mean().reset_index()
 avg_availability.columns = ['index_right', 'avg_availability']
@@ -53,21 +52,24 @@ avg_availability.columns = ['index_right', 'avg_availability']
 avg_price2 = geo_data2.groupby('index_right')['price_per_m2'].mean().reset_index()
 avg_price2.columns = ['index_right', 'price_per_m2']
 
-# Merge the average prices and availability with the GeoDataFrame
-gdf = gdf.merge(avg_price1, left_index=True, right_on='index_right', how='left')
-gdf = gdf.merge(avg_availability, left_index=True, right_on='index_right', how='left')
-gdf = gdf.merge(avg_price2, left_index=True, right_on='index_right', how='left', suffixes=('_listings', '_loyer'))
+
+# Merge the average prices and availability with the GeoDataFrame for listings
+gdf_listings = gdf.merge(avg_price1, left_index=True, right_on='index_right', how='left')
+gdf_listings = gdf_listings.merge(avg_availability, left_index=True, right_on='index_right', how='left', suffixes=('', '_availability'))
+gdf_listings = gdf_listings.merge(avg_price1_basics, left_index=True, right_on='index_right', how='left', suffixes=('', '_price1_basics'))
+
+gdf_loyer = gdf.merge(avg_price2, left_index=True, right_on='index_right', how='left')
 
 # Create the first map
-fig1 = px.choropleth_mapbox(gdf, geojson=gdf.geometry, locations=gdf.index, color='price_per_m2_pondered',
+fig1 = px.choropleth_mapbox(gdf_listings, geojson=gdf.geometry, locations=gdf.index, color='price_per_m2_pondered',
                             mapbox_style="carto-positron", center={"lat": 48.8566, "lon": 2.3522}, zoom=11,
                             opacity=0.5, hover_name='l_ar', labels={'price_per_m2_pondered': 'Avg Price Listings'},
                             hover_data={'avg_availability': ':.2f', 'price_per_m2': ':.2f', 'index_right': False})
 
 # Create the second map
-fig2 = px.choropleth_mapbox(gdf, geojson=gdf.geometry, locations=gdf.index, color='price_per_m2',
+fig2 = px.choropleth_mapbox(gdf_loyer, geojson=gdf.geometry, locations=gdf.index, color='price_per_m2',
                             mapbox_style="carto-positron", center={"lat": 48.8566, "lon": 2.3522}, zoom=11,
-                            opacity=0.5, hover_name='l_ar', labels={'price_per_m2_loyer': 'Avg Price Loyer'})
+                            opacity=0.5, hover_name='l_ar', labels={'price_per_m2': 'Avg Price Loyer'})
 
 # Define the layout of the app
 layout = html.Div([
